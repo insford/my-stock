8d109474ec3ab06deeefecd2a192f035a3759a19
insford <insford@users.noreply.github.com>
2026-08-19 08:52:38 +0000

    Auto-update live_market.js market prices [skip ci]

--- guide/data/live_market.js
+++ guide/data/live_market.js
@@ -1,9 +1,9 @@
 window.LIVE_MARKET_DATA = {
-  "last_updated": "2026-08-19 17:05:50",
+  "last_updated": "2026-08-19 17:52:37",
   "prices": {
     "kospi": 6471.17,
-    "samsung": 261000,
-    "hynix": 1647000,
+    "samsung": 260000,
+    "hynix": 1641000,
     "cd": 1075115,
     "sofr": 60615,
     "us30b": 7110,
