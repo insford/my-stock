65bec09b472c3b9474c4e895fb0058145759aa28
insford <2253129+insford@users.noreply.github.com>
2026-09-11 19:22:22 +0000

    Auto-update live market prices & history sqlite db [skip ci]

--- guide/data/live_market.js
+++ guide/data/live_market.js
@@ -1,5 +1,5 @@
 window.LIVE_MARKET_DATA = {
-  "last_updated": "2026-09-12 01:50:09",
+  "last_updated": "2026-09-12 04:22:13",
   "prices": {
     "kospi": 6909.91,
     "samsung": 262000,
@@ -10,14 +10,14 @@ window.LIVE_MARKET_DATA = {
     "gold": 26490,
     "snp500": 25440,
     "us10b": 11070,
-    "usdkrw": 1340.14,
-    "tsla": 365.25,
-    "nvda": 219.0,
-    "spcx": 148.55,
-    "spacex": 148.55,
-    "googl": 340.47,
-    "mu": 973.57,
-    "qqqm": 294.66,
-    "tltw": 20.91
+    "usdkrw": 1341.47,
+    "tsla": 364.7,
+    "nvda": 218.98,
+    "spcx": 149.96,
+    "spacex": 149.96,
+    "googl": 338.94,
+    "mu": 975.19,
+    "qqqm": 294.69,
+    "tltw": 20.86
   }
 };
